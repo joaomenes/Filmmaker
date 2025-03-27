23e0673a9de451dd33caa261bd1d4f86c01b3ab5
Joao <joao.menes@hotmail.com>
2025-03-26 23:46:08 -0300

    Filtragem de anos dinamicamente

--- homePage/urls.py
+++ homePage/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from homePage.views import index, filter_search  
+from homePage.views import index, filter_search
 
 
 urlpatterns = [
     path('', index, name='index'),  
     path('filter/', filter_search, name='filter'),  
-    ]
+]
--- homePage/views.py
+++ homePage/views.py
@@ -3,16 +3,21 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt  
 from homePage.service import search_movies_or_series  
+from datetime import datetime
 
 def index(request):
-    return render(request, 'homePage/index.html')
+    
+    current_year = datetime.now().year
+    year_range = range(1975, current_year + 1)
+
+    return render(request, 'homePage/index.html', {'year_range': year_range})
 
 @csrf_exempt  
 def filter_search(request):
+    
     if request.method == 'POST':
         try:
             data = json.loads(request.body) 
-            
            
             resultados = search_movies_or_series(data)
 
@@ -22,3 +27,4 @@ def filter_search(request):
             return JsonResponse({"error": str(e)}, status=500)
     
     return JsonResponse({"error": "Método não permitido"}, status=405)
+
